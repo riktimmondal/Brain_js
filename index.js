a7b38d76f3ac72ab136008e6e9f89ed8daa75325
Riktim Mondal <riktimrules@gmail.com>
2019-01-25 13:58:19 +0530

    Xor net

---
+++ index.js
@@ -0,0 +1,12 @@
+//crreating a XOR net
+
+const net=new brain.NeuralNetwork({ hiddenLayers: [3] });
+const trainingData=[
+    {input:[0,0],output:[0]},
+    {input:[0,1],output:[1]},
+    {input:[1,0],output:[1]},
+    {input:[1,1],output:[1]}
+];
+
+net.train(trainingData);
+console.log(net.run([1,0]));
